34a3814159a60a13e62d05a8bccfb8e168d9ba47
{Aarti} <aartijangra482@gmail.com>
2024-01-30 01:11:07 +0530

    first commit

---
+++ scannerexample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class scannerexample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("enter name: ");
+        String name = sc.nextLine();
+        System.out.println("name is : "+ name);
+    }
+}
